7f365b1245d764856ea3bbe987046f8ee5d0803a
Imran Mirza <imranmirza070901@gmail.com>
2023-01-29 19:48:24 +0530

    webpack file changes-2

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,30 +1,18 @@
 import webpack from "webpack";
-import path from "path";
-import { fileURLToPath } from "url";
 import { resolve } from "path";
 import { readdirSync } from "fs";
 var nodeModules = {};
 
-// note the path.resolve(__dirname, ...) part
-// without it, eslint-import-resolver-webpack fails
-// since eslint might be invoked with different cwd
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
-readdirSync(resolve(__dirname, "node_modules"))
-	.filter((x) => [".bin"].indexOf(x) === -1)
-	.forEach((mod) => {
-		nodeModules[mod] = `commonjs ${mod}`;
-	});
 
-// es5 style alternative
-// fs.readdirSync(path.resolve(__dirname, 'node_modules'))
-//     .filter(function(x) {
-//         return ['.bin'].indexOf(x) === -1;
-//     })
-//     .forEach(function(mod) {
-//         nodeModules[mod] = 'commonjs ' + mod;
-//     });
+
+fs.readdirSync(path.resolve(__dirname, 'node_modules'))
+    .filter(function(x) {
+        return ['.bin'].indexOf(x) === -1;
+    })
+    .forEach(function(mod) {
+        nodeModules[mod] = 'commonjs ' + mod;
+    });
 
 export const name = "server";
 export const target = "node";
